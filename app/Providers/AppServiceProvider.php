6aa082981e35fe933792ec758ac8ecbac2330f07
muhammadesg <132346513+muhammadesg@users.noreply.github.com>
2026-01-08 13:07:55 +0500

    Fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         view()->composer('site.layouts.site', function ($view) {
             $setting = Setting::first();
             $view->with(compact('setting'));
